cf3f5da27748416424599eea65b16369012f3a47
Kheih <56195049+Kheih@users.noreply.github.com>
2020-11-28 21:19:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<table>
+<tr>
+  <th>
+<canvas id="myCanvas" width="200" height="200" style="border:1px solid #000000;"
+</canvas>
+ </th>
+  <th>
+<canvas id="myCanvas1" width="200" height="200" style="border:1px solid #000000;"
+</canvas>
+ </th>
+   <th>
+<canvas id="myCanvas2" width="200" height="200" style="border:1px solid #000000;"
+</canvas>
+ </th>
+   <th>
+<canvas id="myCanvas3" width="200" height="200" style="border:1px solid #000000;"
+</canvas>
+ </th>
+</tr>
+  
+  <tr>
+  <th>
+<canvas id="myCanvas4" width="200" height="200" style="border:1px solid #000000;"
+</canvas>
+ </th>
+  <th>
+<canvas id="myCanvas5" width="200" height="200" style="border:1px solid #000000;"
+</canvas>
+ </th>
+   <th>
+<canvas id="myCanvas6" width="200" height="200" style="border:1px solid #000000;"
+</canvas>
+ </th>
+   <th>
+<canvas id="myCanvas7" width="200" height="200" style="border:1px solid #000000;"
+</canvas>
+ </th>
+</tr>
+  </table>
+
+</body>
+
+ <script src="js/myscripts.js"></script>
+</html>
